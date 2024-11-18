0f0a8dc77e9256c214069207204d567c160dd830
GabrielReboli <gabrielreboli@gmail.com>
2024-11-18 10:05:29 -0300

    Delete app/supabaseClient.ts

--- app/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = '<sua-url-supabase>';
-const SUPABASE_ANON_KEY = '<sua-chave-anonima>';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
