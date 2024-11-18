10f8a5c4d5bab8765607b63ab8ae3028aa720904
GabrielReboli <gabrielreboli@gmail.com>
2024-11-18 09:41:16 -0300

    Update supabaseClient.ts

--- app/supabaseClient.ts
+++ app/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://wzlpsrikbtfumaqpgflr.supabase.co';
+const SUPABASE_URL = '<sua-url-supabase>';
 const SUPABASE_ANON_KEY = '<sua-chave-anonima>';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
